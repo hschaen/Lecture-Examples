b7a949a76ecedd896dac723eccbf7c36e28d2ccf
sjproctor <sproctor950@gmail.com>
2020-03-13 00:06:45 -0700

    external api buildout

--- external-api-fetch/README.md
+++ external-api-fetch/README.md
@@ -17,3 +17,41 @@
 - Signed up for [New York Times Developers](https://developer.nytimes.com/apis)
 - Confirmed email, signed in, added app in the user dropdown menu, registered app
 - Selected the API and save, generates the key
+
+
+## API Key
+- Create a file called *.env* in the outermost level of the react app
+- Save the API key using the format `REACT_APP_MY_KEY_NAME_HERE = "my-api-key"`
+- `REACT_APP_` is the key phrase needed for React
+- Add the *.env* file to the *.gitignore* file
+```
+# api keys
+.env
+```
+- Call the api key in the appropriate component
+```
+let apiKey = process.env.REACT_APP_MY_KEY_NAME_HERE
+```
+
+
+## Fetch / then
+- Mean Girls video
+- Asynchronous responses
+- Async/Await
+- Washing machine example
+
+```
+let endpoint = 'https://url//apikey'
+  fetch(endpoint)
+  .then((response => {
+    return response.json()
+  }))
+  .then((payload=>{
+    this.setState({ payload })
+  }))
+```
+- Fetch sends the request which returns a promise
+- .then() gets the response from the API and returns it as JSON
+- When the first promise gets resolved, we get the received data/payload and can set it to state
+
+-
--- external-api-fetch/src/App.js
+++ external-api-fetch/src/App.js
@@ -1,77 +1,70 @@
 import React, { Component } from "react"
 import './App.css'
-import { InputGroup, InputGroupAddon, Input, Jumbotron, Container, Alert, Button } from 'reactstrap'
+import { InputGroup, InputGroupAddon, Input, Jumbotron, Container, Alert, Button, ListGroup, ListGroupItem } from 'reactstrap'
 
 class App extends Component{
   constructor(){
     super()
     this.state = {
       search: "", // Search can start with an empty string and be updated from the form
-      review: "",
-      title: "",
-      author: "",
+      articles: [],
       error: null // If we have an error from the API return it here
     }
   }
 
-  getBookReviews = (query) => {
-    const apiKey = '2KJ3BY5sKw3JX1ZmnHL13YzclFAi0Arp'
-    let searchUrl = `https://api.nytimes.com/svc/books/reviews.json?title=${query}&api-key=${apiKey}`
-    fetch(searchUrl) //Fetch returns a promise
+  getArticles = (query) => {
+    // Api key hidden in another file
+    let apiKey = process.env.REACT_APP_NYT_API_KEY
+    // Setting up the query and passing in the api key to the url
+    let searchUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&api-key=${apiKey}`
+
+    fetch(searchUrl) // Fetch returns a promise
     .then((response) => {
       if(response.status !== 200){
         throw({ message: "Could not perform search. Please try again." })
       }
-      return response.json() // Return the response as JSON
+        return response.json() // Return the response as JSON
     })
     .then((payload) => {
-      // the payload is a larger object and needs to be parsed
-      const title = payload.body.results[0].book_title
-      const author = payload.body.results[0].book_author
-      const review = payload.body.results[0].url
-      console.log(review)
-      this.setState({ review, author, title }) //Finally, we can add the found recipes to our list
-      // testing -------
-      // fetch(review)
-      // .then((response) => {
-      //   console.log("here", response)
-      //   if(response.status !== 200){
-      //     throw({ message: "Could not perform search. Please try again." })
-      //   }
-      //   return response.json() // Return the response as JSON
-      // })
-      // // testing -------
+      console.log(payload.response.docs)
+      let articles = payload.response.docs
+      this.setState({ articles: articles })
+      console.log(this.state.articles)
     })
-    .catch((error) => this.setState({ error }))
+    .catch((error) => this.setState({error}))
   }
-  search = () => {
-    this.getBookReviews(this.state.search) // When a user clicks 'search', re-send the request to the api, with search values
-  }
+
+    search = () => {
+      this.getArticles(this.state.search) // When a user clicks 'search', re-send the request to the api, with search values
+    }
 
   render(){
     return(
       <React.Fragment>
+
         <div>
           <Jumbotron fluid>
             <Container fluid>
-              <h1 className="display-3">Book Reviews App</h1>
-              <p className="lead">Get NYT book reviews here.</p>
+              <h1 className="display-3">News Articles</h1>
+              <p className="lead">Get NYT articles here.</p>
             </Container>
           </Jumbotron>
         </div>
+
         <div id="input">
           <InputGroup>
             <Input
-              type="text"
-              placeholder="search for a book title"
-              onChange={ (e) => this.setState({ search: e.target.value }) }
-              value={ this.state.search }
+            type="text"
+            placeholder="search for an article"
+            onChange={ (e) => this.setState({ search: e.target.value }) }
+            value={ this.state.search }
             />
-            <InputGroupAddon addonType="append">
-            <Button color="secondary" onClick={ this.search }>Search</Button>
+              <InputGroupAddon addonType="append">
+              <Button color="secondary" onClick={ this.search }>Search</Button>
             </InputGroupAddon>
           </InputGroup>
         </div>
+
         <div id="error">
           { this.state.error &&
             <Alert color="danger">
@@ -79,19 +72,20 @@ class App extends Component{
             </Alert>
           }
         </div>
-        <div>
-          { this.state.review &&
-            <Jumbotron fluid>
-              <Container fluid>
-                <h4 className="lead">Title: { this.state.title }</h4>
-                <h4 className="lead">Author: { this.state.author }</h4>
-                <p className="lead">{ this.state.review }</p>
-              </Container>
-            </Jumbotron>
+
+        <div id="results">
+          { this.state.articles.map((article, index) => {
+              return(
+                <ListGroup key={ index } >
+                  <ListGroupItem tag="a" href={ article.web_url } action>{ article.abstract }</ListGroupItem>
+                </ListGroup>
+              )
+            })
           }
         </div>
+
       </React.Fragment>
     )
   }
 }
-export default App
+  export default App
